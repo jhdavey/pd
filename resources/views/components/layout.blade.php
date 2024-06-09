b0edfca3de187ef86cae871d10c6568e24f36902
jhdavey <jhdavey5182@gmail.com>
2024-06-09 15:48:06 -0400

    favicon 2

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Passion Driven</title>
 
-    <!-- Google Font: Work Sans -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+</head>
 
-    <!-- Alpine.js -->
-    <script src="//unpkg.com/alpinejs" defer></script>
+<!-- Google Font: Work Sans -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <!-- Lightbox for image expansion -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css" rel="stylesheet">
+<!-- Alpine.js -->
+<script src="//unpkg.com/alpinejs" defer></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
+<!-- Lightbox for image expansion -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css" rel="stylesheet">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
 
 </head>
 
@@ -30,11 +36,11 @@
                         <img class="h-[50px] mr-10" src="{{ Vite::asset('resources/images/logoFull.png') }}" />
                     </a>
                     @auth
-                        <div class="hidden lg:flex text-lg transition-colors duration-200 space-x-6">
-                            <a href="/" class="hover:text-gray-500">Browse</a>
-                            <a href="{{ route('garage.show', $authUser->id) }}" class="hover:text-gray-500">Garage</a>
-                            <a href="/feedback" class="hover:text-gray-500">Beta</a>
-                        </div>
+                    <div class="hidden lg:flex text-lg transition-colors duration-200 space-x-6">
+                        <a href="/" class="hover:text-gray-500">Browse</a>
+                        <a href="{{ route('garage.show', $authUser->id) }}" class="hover:text-gray-500">Garage</a>
+                        <a href="/feedback" class="hover:text-gray-500">Beta</a>
+                    </div>
                     @endauth
                 </div>
 
@@ -43,15 +49,15 @@
                         <x-forms.input :label="false" name="q" placeholder="Search builds..."></x-forms.input>
                     </x-forms.form>
                     @guest
-                        <a href="/register" class="hover:text-gray-500">Sign Up</a>
-                        <a href="/login" class="hover:text-gray-500">Log In</a>
+                    <a href="/register" class="hover:text-gray-500">Sign Up</a>
+                    <a href="/login" class="hover:text-gray-500">Log In</a>
                     @endguest
-                    @auth                        
-                        <form method="POST" action="/logout">
-                            @csrf
-                            @method('DELETE')
-                            <button class="hover:text-gray-500">Log Out</button>
-                        </form>
+                    @auth
+                    <form method="POST" action="/logout">
+                        @csrf
+                        @method('DELETE')
+                        <button class="hover:text-gray-500">Log Out</button>
+                    </form>
                     @endauth
                 </div>
 
@@ -67,27 +73,27 @@
             <!-- Mobile Menu -->
             <div x-show="open" @click.away="open = false" class="lg:hidden">
                 <div class="pt-4 pb-2 space-y-1">
-                    
+
                     <x-forms.form action="/search">
                         <x-forms.input class="h-4 w-full sm:w-['100px']" :label="false" name="q" placeholder="Search Year/Make/Model"></x-forms.input>
                     </x-forms.form>
 
                     @auth
-                        <a href="/" class="block px-4 py-2 text-sm font-semibold hover:bg-white/10">Home</a>
-                        <a href="/garage" class="block px-4 py-2 text-sm font-semibold hover:bg-white/10">Garage</a>
-                        <a href="/feedback" class="block px-4 py-2 text-sm font-semibold hover:bg-white/10">Beta</a>
+                    <a href="/" class="block px-4 py-2 text-sm font-semibold hover:bg-white/10">Home</a>
+                    <a href="/garage" class="block px-4 py-2 text-sm font-semibold hover:bg-white/10">Garage</a>
+                    <a href="/feedback" class="block px-4 py-2 text-sm font-semibold hover:bg-white/10">Beta</a>
                     @endauth
 
                     @guest
-                        <a href="/register" class="block px-4 py-2 text-sm font-semibold hover:bg-white/10">Sign Up</a>
-                        <a href="/login" class="block px-4 py-2 text-sm font-semibold hover:bg-white/10">Log In</a>
+                    <a href="/register" class="block px-4 py-2 text-sm font-semibold hover:bg-white/10">Sign Up</a>
+                    <a href="/login" class="block px-4 py-2 text-sm font-semibold hover:bg-white/10">Log In</a>
                     @endguest
                     @auth
-                        <form method="POST" action="/logout">
-                            @csrf
-                            @method('DELETE')
-                            <button class="w-full text-left px-4 py-2 text-sm font-semibold hover:bg-white/10">Log Out</button>
-                        </form>
+                    <form method="POST" action="/logout">
+                        @csrf
+                        @method('DELETE')
+                        <button class="w-full text-left px-4 py-2 text-sm font-semibold hover:bg-white/10">Log Out</button>
+                    </form>
                     @endauth
                 </div>
             </div>
@@ -98,4 +104,5 @@
         </main>
     </div>
 </body>
+
 </html>
