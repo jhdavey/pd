b8b4bdbf97b08ee000757d8701ecca457b375c8b
jhdavey <jhdavey5182@gmail.com>
2024-06-08 19:19:02 -0600

    Editing email

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,4 +1,3 @@
-<x-mail::message>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,14 +8,17 @@
             font-family: Arial, sans-serif;
             background-color: #f8f9fa;
             color: #343a40;
+            margin: 0;
+            padding: 0;
         }
-        .container {
+        .wrapper {
             width: 100%;
             padding: 20px;
             background-color: #ffffff;
             border: 1px solid #dee2e6;
             border-radius: 5px;
-            margin-top: 20px;
+            margin: 20px auto;
+            max-width: 600px;
         }
         .header {
             text-align: center;
@@ -43,65 +45,16 @@
     </style>
 </head>
 <body>
-    <div class="container">
+    <div class="wrapper">
         <div class="header">
             {{ config('app.name') }}
         </div>
         <div class="content">
-            {{-- Greeting --}}
-            @if (! empty($greeting))
-                <h1>{{ $greeting }}</h1>
-            @else
-                @if ($level === 'error')
-                    <h1>@lang('Whoops!')</h1>
-                @else
-                    <h1>@lang('Hello!')</h1>
-                @endif
-            @endif
-
-            {{-- Intro Lines --}}
-            @foreach ($introLines as $line)
-                <p>{{ $line }}</p>
-            @endforeach
-
-            {{-- Action Button --}}
-            @isset($actionText)
-                <?php
-                    $color = match ($level) {
-                        'success', 'error' => $level,
-                        default => 'primary',
-                    };
-                ?>
-                <a href="{{ $actionUrl }}" class="btn-primary">
-                    {{ $actionText }}
-                </a>
-            @endisset
-
-            {{-- Outro Lines --}}
-            @foreach ($outroLines as $line)
-                <p>{{ $line }}</p>
-            @endforeach
-
-            {{-- Salutation --}}
-            @if (! empty($salutation))
-                <p>{{ $salutation }}</p>
-            @else
-                <p>@lang('Regards'),<br>{{ config('app.name') }}</p>
-            @endif
+            {{ $slot }}
         </div>
-        {{-- Subcopy --}}
-        @isset($actionText)
-            <div class="footer">
-                @lang(
-                    "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-                    'into your web browser:',
-                    [
-                        'actionText' => $actionText,
-                    ]
-                ) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
-            </div>
-        @endisset
+        <div class="footer">
+            © {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
+        </div>
     </div>
 </body>
 </html>
-</x-mail::message>
--- resources/views/vendor/notifications/message.blade.php
+++ resources/views/vendor/notifications/message.blade.php
@@ -1,13 +1,57 @@
-<x-layout>
-    <div class="wrapper">
-        <div class="content">
-            <div class="header">
-                <h1>{{ config('app.name') }}</h1>
-            </div>
-            {{ $slot }}
-            <div class="footer">
-                © {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
-            </div>
-        </div>
-    </div>
-</x-layout>
+@extends('vendor.notifications.email')
+
+@section('content')
+{{-- Greeting --}}
+@if (! empty($greeting))
+<h1>{{ $greeting }}</h1>
+@else
+@if ($level === 'error')
+<h1>@lang('Whoops!')</h1>
+@else
+<h1>@lang('Hello!')</h1>
+@endif
+@endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+<p>{{ $line }}</p>
+@endforeach
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    $color = match ($level) {
+        'success', 'error' => $level,
+        default => 'primary',
+    };
+?>
+<a href="{{ $actionUrl }}" class="btn-primary">
+{{ $actionText }}
+</a>
+@endisset
+
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+<p>{{ $line }}</p>
+@endforeach
+
+{{-- Salutation --}}
+@if (! empty($salutation))
+<p>{{ $salutation }}</p>
+@else
+<p>@lang('Regards'),<br>{{ config('app.name') }}</p>
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+<p>
+@lang(
+    "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
+    'into your web browser:',
+    [
+        'actionText' => $actionText,
+    ]
+) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+</p>
+@endisset
+@endsection
