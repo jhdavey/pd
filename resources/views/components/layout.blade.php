dd81a799799e4c1aebb7168177a8a0aa8f9ffc81
jhdavey <jhdavey5182@gmail.com>
2024-06-11 12:55:11 -0400

    analytics.blade.php with include('analytics') in layout head test

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,19 +2,7 @@
 <html lang="en">
 
 <head>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CMMJM3RYNE"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-CMMJM3RYNE');
-    </script>
-
+  
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Passion Driven</title>
@@ -43,6 +31,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
 
+    @include('analytics')
 </head>
 
 <body class="bg-background text-white font-sans pb-12">
