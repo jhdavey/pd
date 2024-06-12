343883f64899c6e156ebb2049061eca3e5a0f27f
jhdavey <jhdavey5182@gmail.com>
2024-06-11 21:32:45 -0400

    Editing exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -14,7 +15,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        // ...
     ];
 
     /**
@@ -50,14 +51,21 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        if ($exception instanceof HttpException) {
-            if ($exception->getStatusCode() == 404) {
-                return response()->view('errors.404', [], 404);
+        if ($this->isHttpException($exception)) {
+            $statusCode = $exception->getStatusCode();
+            if (view()->exists("errors.{$statusCode}")) {
+                return response()->view("errors.{$statusCode}", [], $statusCode);
+            }
+        } else {
+            if (config('app.debug')) {
+                return parent::render($request, $exception);
             }
 
-            if ($exception->getStatusCode() == 500) {
-                return response()->view('errors.500', [], 500);
-            }
+            // Log the exception for debugging
+            \Log::error($exception);
+            
+            // Show custom 500 error page
+            return response()->view('errors.500', [], 500);
         }
 
         return parent::render($request, $exception);
