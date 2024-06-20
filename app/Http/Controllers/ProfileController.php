71634b247ced504c34c9da51646eaff07d2ba129
jhdavey <jhdavey5182@gmail.com>
2024-06-19 21:07:58 -0400

    Added auth to user profile edit view

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,10 +11,10 @@ class ProfileController extends Controller
 {
     public function show(User $user)
     {
-        if (Auth::check() && Auth::id() === $user->id) {
-            return view('profile.edit', compact('user'));
-        } else {
-            abort(403, 'Unauthorized action.');
+        if (Auth::check()) {
+            $user = Auth::user();
+
+            return view('profile', compact('user'));
         }
     }
 
