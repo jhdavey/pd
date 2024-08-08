0b9e41d8d29da21f6cd6ddb8fdbf74f0479c25f2
jhdavey <jhdavey5182@gmail.com>
2024-08-08 18:48:11 -0400

    updated feedback page info

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -4,22 +4,20 @@
 
     <x-page-heading>Welcome to the Passion Driven Beta!</x-page-heading>
 
-    <p class="my-6">We am constantly adding and improving features, but there is still a lot of work to do and some bugs to fix. Here are some exciting features we are currently working on:</p>
+    <p class="my-6">My name is Harley and I am building this project for fun. I am constantly adding and improving features, but there is still a lot of work to do and some bugs to fix. Here are some exciting features I are currently working on:</p>
 
     <ul class="my-6 space-y-3">
         <li>-Video, and file uploads</li>
-        <li>-Improved build notes editor</li>
-        <li>-Adding track times</li>
+        <li>-Improved build notes editor with image and file uploads</li>
         <li>-Build sheet exporting to pdf and excel files</li>
         <li>-Maintenance tracking</li>
         <li>-Communty forums</li>
         <li>-iOS, Android apps</li>
-        <li>-Bug fixes, and much much more!</li>
     </ul>
 
-    <p class="my-6">We want to hear from you! Please share any feedback you have, and let us know what features you would like to see.</p>
+    <p class="my-6">I'd love to hear from you! Please share any feedback you have, and let me know what features you would like to see. If you are a developer interested in helping wth this project I would love to speak to you! Just message here.</p>
 
-    <p class="my-6">Thank you for being part of the community!</p>
+    <p class="my-6">Thank you for your support and for helping create this community!</p>
 
     <x-forms.divider />
 
