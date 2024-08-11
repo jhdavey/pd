347df1685f9e40689d893064ace72ba6d7d303a9
jhdavey <jhdavey5182@gmail.com>
2024-08-10 20:45:35 -0400

    testing different word doc controller

--- app/Http/Controllers/BuildController.php
+++ app/Http/Controllers/BuildController.php
@@ -348,23 +348,23 @@ class BuildController extends Controller
         // Add modifications
         $section->addText('Modifications:', ['bold' => true]);
 
-        if ($build->modifications->isEmpty()) {
-            $section->addText('No modifications found.');
-        } else {
-            foreach ($build->modifications as $mod) {
-                if (!is_null($mod->category) && !is_null($mod->brand) && !is_null($mod->name)) {
-                    $section->addText("Category: {$mod->category}");
-                    $section->addText("Brand: {$mod->brand}");
-                    $section->addText("Name: {$mod->name}");
-                    $section->addText("Price: {$mod->price}");
-                    $section->addText("Part Number: {$mod->part_number}");
-                    $section->addText("Notes: {$mod->notes}");
-                    $section->addTextBreak(1);
-                } else {
-                    $section->addText('Some modification details are missing.');
-                }
-            }
-        }
+        // if ($build->modifications->isEmpty()) {
+        //     $section->addText('No modifications found.');
+        // } else {
+        //     foreach ($build->modifications as $mod) {
+        //         if (!is_null($mod->category) && !is_null($mod->brand) && !is_null($mod->name)) {
+        //             $section->addText("Category: {$mod->category}");
+        //             $section->addText("Brand: {$mod->brand}");
+        //             $section->addText("Name: {$mod->name}");
+        //             $section->addText("Price: {$mod->price}");
+        //             $section->addText("Part Number: {$mod->part_number}");
+        //             $section->addText("Notes: {$mod->notes}");
+        //             $section->addTextBreak(1);
+        //         } else {
+        //             $section->addText('Some modification details are missing.');
+        //         }
+        //     }
+        // }
 
         // Add notes
         // $section->addText('Build Notes:', ['bold' => true]);
